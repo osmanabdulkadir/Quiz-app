cde365946be6a17d3dafe43a9ae8d03afb117f6d
osman abdul <74805866+osmanabdulkadir@users.noreply.github.com>
2021-08-30 21:42:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,119 +1,67 @@
-/* what do we want our code to do>
-1. check if a answer is correct or not correct 
-2.update the DOM with a new question and answers.
-3. reset when all the questions are done
-
-1. grab all elements from the dom.
-    the qestion.
-    the answers.
-    the body- to chagne colors
-
-2. we need to use a object to store our answers and questions.
+const startButton = document.getElementById("start-btn");
+const question = document.getElementById("question");
+const answerConatiner = document.getElementById("answer-container");
+const nextButton = document.getElementById("next-btn");
 
 
-3. bulid a function that checks the question if it is rigth it sets the body to the wrong color and then calls a function that updates the dom to the next question and adds new answers
+function startGame() {
+    startButton.classList.add("hide")
+    question.classList.remove('hide')
+    answerConatiner.classList.remove('hide')
+    nextButton.classList.remove('hide')
+; 
+    loadNextQuestion
+}
 
-4. add event listeners on all buttons to check if they are the correct answers
-
-
-    loop throught the object; 
-    while 
-    creat a varbaile 
-    fuction
-    set timeOUT
-*/
-
-const body = document.querySelector("body")
-
-let choice1 = document.getElementById("answer-1");
-let choice2 = document.getElementById("answer-2");
-let choice3 = document.getElementById("answer-3");
-let choice4 = document.getElementById("answer-4");
-
-let question = document.getElementById("question");
-
-let score = 0;
-
-let answersOBJ = [
-    {question: "Who's is the 7th Hokage",
-     "options": ["Naruto","Sasuke","Garaa","Kakashi"],
-      answer : "Naruto"},
-
-    {question: "Who's is the Strongest Uchicha",
-     "options": ["Zetzu","Sasuke","Madara","Kakashi"],
-      answer : "Madara"},
-
-    {question: "Who's is the 1st Hokage",
-     "options": ["Naruto","Hashirama","Toborama","Rock Lee"],
-     answer : "Hashirama"},
-
-    {question: "Naruto is a member of which clan?",
-     "options": ["Senju","Uchiha","Uzumaki","Hugya"],
-     answer : "Uzumaki"},
-
-    {question:
-         "How many Tailed Beasts (Biju) existed?",
-          "options": ["1","8","9","10"],
-           answer : '10'},
-];
-
-
-//event Listeners
-
-
-choice1.addEventListener("click", checkAnswer);
- choice2.addEventListener("click", checkAnswer);
- choice3.addEventListener("click", checkAnswer);
- choice4.addEventListener("click", checkAnswer);
-
- function checkAnswer() {
-     let score = 0
-     
-
-     let chociesArr = [choice1.innerText,choice2.innerText,choice3.innerText,choice4.innerText,]
-    while(score <= 4){
-        
-        
-
-         for(var i=0; i<chociesArr.length; i++){
-            if(chociesArr[i] == answersOBJ[score]["answer"]){
-                 body.classList.add("right")
-                 console.log(score)
-                
-                 setTimeout(function(){body.classList.remove("right");}, 1500)
-                question.innerText = answersOBJ[score][question];
-                 choice1.innerText = answersOBJ[score]["options"][0];
-                choice2.innerText= answersOBJ[score]["options"][1];
-                 choice3.innerText= answersOBJ[score]["options"][2];
-               choice4.innerText= answersOBJ[score]["options"][3];
-               
-
-             } else if(chociesArr[i] != answersOBJ[score]["answer"]){
-                 body.classList.add("wrong")
-                 console.log(score)
-                 setTimeout(function(){body.classList.remove("wrong");}, 1500)
-                 question.innerText = answersOBJ[score][question]
-                 choice1.innerText= answersOBJ[score]["options"][0]
-                 choice2.innerText= answersOBJ[score]["options"][1]
-                 choice3.innerText= answersOBJ[score]["options"][2]
-               choice4.innerText= answersOBJ[score]["options"][3]
-              
-             }
-            
-            
-      
+ function loadNextQuestion() {
+     question.innerText = questionArr.question
+     question.answers.forEach(answers => {
+         const button = document.createElement('button')
+         button.classList.add('answer');
+         answerConatiner.append.button
+         button.innerText = answers.text
          
-        
-   
-    
-    
-    
-}
+     });
 
-}
-score++;
  }
- score++;
 
+const questionArr = [
+    {
+        question: "Who is Kanye's wife",
+        answers: [
+            {text:"Kim Kardashian", correct:true},
+            {text:"Kris Kardashian", correct:false},
+            {text:"Amber Rose", correct:false},
+            {text:"Michelle Obama", correct:false}
+        ],
+    },
+    {
+        question: "Who is Kanye's idol",
+        answers: [
+            {text:"Jay Z", correct:true},
+            {text:"Drake", correct:false},
+            {text:"Nas", correct:false},
+            {text:"Tupac", correct:false}
+        ]
+    },
+    {
+        question: "whats Kanye's Clothing Brand called",
+        answers: [
+            {text:"Pastelle", correct:false},
+            {text:"Donda", correct:false},
+            {text:"Yeezy", correct:true},
+            {text:"Adidas", correct:false}
+        ]
+    },
+    {
+        question: "what's Kanye's best Album",
+        answers: [
+            {text:"MDBTF", correct:true},
+            {text:"YE", correct:true},
+            {text:"Gradution", correct:true},
+            {text:"Yeezus", correct:true}
+        ]
+    }
+]
 
-
+startButton.addEventListener("click", startGame)
